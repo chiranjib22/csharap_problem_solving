5b88ff6199711268a64e7904de01d13403fdf161
Chiranjib Chakraborty <chiranjib@akijbashir.com>
2025-12-10 10:25:03 +0600

    Striver DSA sheet solve begins

---
+++ src/solve/Largest_Element_in_a_Array/Program.cs
@@ -0,0 +1,10 @@
+﻿string input = Console.ReadLine() ?? "";
+int[] numbers = Array.ConvertAll(input.Split(','), int.Parse);
+
+int max = int.MinValue;
+foreach(int number in numbers)
+{
+    if(number > max) max = number;
+}
+Console.WriteLine(max);
+
