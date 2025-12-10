4d3e5a2b41083e113225ccb06338aca5f373de9c
Chiranjib Chakraborty <chiranjib@akijbashir.com>
2025-12-10 11:05:07 +0600

    2 basic problems are solved

---
+++ src/solve/Count_Digits/Program.cs
@@ -0,0 +1,20 @@
+﻿
+#region BRUTEFORCE
+//int number = Convert.ToInt32(Console.ReadLine());
+//int temp = number;
+//int count = 0;
+//while(temp!=0)
+//{
+//    count++;
+//    temp /= 10;
+//}
+//Console.WriteLine(count);
+#endregion
+
+#region OPTIMAL
+int number = Convert.ToInt32(Console.ReadLine());
+int count = (number > 0) ? (int)Math.Log10(number) + 1 : 1;
+Console.WriteLine(count);
+#endregion
+
+
--- src/solve/Largest_Element_in_a_Array/Program.cs
+++ src/solve/Largest_Element_in_a_Array/Program.cs
@@ -1,10 +1,20 @@
-﻿string input = Console.ReadLine() ?? "";
-int[] numbers = Array.ConvertAll(input.Split(','), int.Parse);
+﻿
+#region BRUTE FORCE APPROACH
+int[] numbers = Array.ConvertAll((Console.ReadLine()!.Split(',')), int.Parse);
+Array.Sort(numbers);
+Console.WriteLine(numbers[^1]);
+#endregion
 
-int max = int.MinValue;
-foreach(int number in numbers)
-{
-    if(number > max) max = number;
-}
-Console.WriteLine(max);
+#region OPTIMAL APPROACH
 
+//string input = Console.ReadLine() ?? "";
+//int[] numbers = Array.ConvertAll(input.Split(','), int.Parse);
+
+//int max = int.MinValue;
+//foreach(int number in numbers)
+//{
+//    if(number > max) max = number;
+//}
+//Console.WriteLine(max);
+
+#endregion
