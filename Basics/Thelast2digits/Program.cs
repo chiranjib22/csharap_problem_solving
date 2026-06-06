f18bbfd6d0525ead65f7883717abe674c5af0fce
chiranjib22 <chiranjib.chakraborty.cse@gmail.com>
2026-06-07 01:38:22 +0600

    the last 2 digit was unsolved

--- Basics/Thelast2digits/Program.cs
+++ Basics/Thelast2digits/Program.cs
@@ -2,7 +2,15 @@
  * Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/Y
  */
 
-int[] numbers = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+long[] numbers = Console.ReadLine()!.Split().Select(long.Parse).ToArray();
 
 long result = numbers[0] * numbers[1] * numbers[2] * numbers[3];
+
+Console.WriteLine(result);
+
+string digit = Convert.ToString(result % 10);
+result = result / 10;
+digit = digit + Convert.ToString(10 * (result % 10));
+
+Console.WriteLine(digit);
 
