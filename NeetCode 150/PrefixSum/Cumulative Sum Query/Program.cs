10abb79c570f0bd6019fb71fd3905da2cc8c6948
chiranjib22 <chiranjib.chakraborty.cse@gmail.com>
2026-08-06 14:41:02 +0600

    neetcode started

---
+++ NeetCode 150/PrefixSum/Cumulative Sum Query/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution 
+{
+    public static void Main(String[] args)
+    {
+        int[] number = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+        int[] input = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+        List<int> prefix = new List<int>();
+        prefix.Add(0);
+
+        for(int i = 1; i <= number[0]; i++)
+        {
+             prefix.Add(prefix[i - 1] + input[i - 1]);
+        }
+
+        int query = int.Parse(Console.ReadLine()!);
+
+        for(int i = 0; i<query; i++)
+        {
+            int[] range = Console.ReadLine()!.Split(" ").Select(int.Parse).ToArray();
+
+            int ans = prefix[range[1]+1] - prefix[range[0]];
+            Console.WriteLine(ans);
+        }
+    } 
+
+}
